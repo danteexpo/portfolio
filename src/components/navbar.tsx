f6907cca43ab9b989fb66e6ba96bcedb44c2cda9
Dante <dante0im@gmail.com>
2024-01-04 17:38:25 -0300

    Worked on Layout and Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,44 @@
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
+import { cn } from "@/lib/utils";
 
-const navLinks = ["home", "work", "blog"];
+const navLinks = [
+	{
+		label: "home",
+		pathname: "/",
+		built: true,
+	},
+	{
+		label: "work",
+		pathname: "/work",
+		built: true,
+	},
+	{
+		label: "blog",
+		pathname: "/blog",
+		built: false,
+	},
+];
 
 const Navbar = () => {
 	return (
 		<header className="flex items-center w-full justify-between text-2xl font-medium">
 			<nav className="flex items-center gap-2">
 				{navLinks.map((navLink) => (
-					<Link
-						href={navLink === "home" ? "/" : `/${navLink}`}
-						key={navLink}
-						className="capitalize hover:text-gray-200"
-					>
-						{navLink}
-					</Link>
+					<>
+						<Link
+							href={navLink.pathname}
+							key={navLink.label}
+							className={cn(
+								"capitalize hover:text-gray-700 dark:hover:text-gray-300",
+								!navLink.built &&
+									"pointer-events-none text-gray-500 dark:text-gray-300 line-through decoration-1"
+							)}
+						>
+							{navLink.label}{" "}
+						</Link>
+						{!navLink.built && <p className="inline text-lg">🚧</p>}
+					</>
 				))}
 			</nav>
 			<ModeToggle />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/layout";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
 	HoverCard,
 	HoverCardContent,
@@ -12,16 +13,36 @@ export default function Home() {
 				<h1 className="text-4xl font-bold">Dante Expósito</h1>
 				<p>
 					A self-taught and optimistic frontend developer who loves designing
-					and building web applications using React, Next.js, TailwindCSS, and
-					TypeScript.
+					and building web applications using
+					<HoverCard>
+						<HoverCardTrigger asChild>
+							<a
+								href="https://nextjs.org/"
+								target="_blank"
+								rel="noreferrer"
+								className="hover:underline px-2 decoration-1"
+							>
+								@nextjs,
+							</a>
+						</HoverCardTrigger>
+						<HoverCardContent className="w-80 hidden sm:flex">
+							<div className="flex justify-between space-x-4">
+								<Avatar>
+									<AvatarImage src="https://github.com/vercel.png" />
+									<AvatarFallback>VC</AvatarFallback>
+								</Avatar>
+								<div className="space-y-1">
+									<h4 className="font-semibold">@nextjs</h4>
+									<p>
+										The React Framework – created and maintained by @vercel.
+									</p>
+								</div>
+							</div>
+						</HoverCardContent>
+					</HoverCard>
 				</p>
+				{/* React, Next.js, TailwindCSS, and TypeScript. */}
 			</section>
-			<HoverCard>
-				<HoverCardTrigger>Hover</HoverCardTrigger>
-				<HoverCardContent>
-					The React Framework – created and maintained by @vercel.
-				</HoverCardContent>
-			</HoverCard>
 		</Layout>
 	);
 }
