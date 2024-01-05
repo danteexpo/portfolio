01e341f95b3c8f4568ecaab3ee9fe4de4bbe241f
Dante <dante0im@gmail.com>
2024-01-05 20:29:36 -0300

    Fix <p> error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,50 +38,48 @@ export default function Home() {
 		<Layout>
 			<section>
 				<h1 className="text-4xl font-bold">Dante Expósito</h1>
-				<p>
+				<p className="inline">
 					A self-taught and optimistic frontend developer who loves designing
 					and building web applications using
-					{techs.map((tech, index) => (
-						<>
-							<p className="inline">{index === techs.length - 1 && "and"} </p>
-							<HoverCard key={tech.label}>
-								<HoverCardTrigger asChild>
-									<a
-										href={tech.href}
-										target="_blank"
-										rel="noreferrer"
-										className="hover:underline decoration-1"
-									>
-										@{tech.label}
-									</a>
-								</HoverCardTrigger>
-								<HoverCardContent className="w-80 hidden md:flex">
-									<div className="flex justify-between space-x-4">
-										<Avatar>
-											<AvatarImage
-												src={`/${tech.label}.svg`}
-												alt={tech.label}
-												className={
-													tech.label === "nextjs"
-														? "bg-white border border-white"
-														: ""
-												}
-											/>
-											<AvatarFallback>{tech.label.slice(0, 2)}</AvatarFallback>
-										</Avatar>
-										<div className="space-y-1">
-											<h4 className="font-semibold">@{tech.label}</h4>
-											<p>{tech.description}</p>
-										</div>
-									</div>
-								</HoverCardContent>
-							</HoverCard>
-							<p className="inline">
-								{index === techs.length - 1 ? "." : ","}{" "}
-							</p>
-						</>
-					))}
 				</p>
+				{techs.map((tech, index) => (
+					<>
+						<p className="inline">{index === techs.length - 1 && "and"} </p>
+						<HoverCard key={tech.label}>
+							<HoverCardTrigger asChild>
+								<a
+									href={tech.href}
+									target="_blank"
+									rel="noreferrer"
+									className="hover:underline decoration-1"
+								>
+									@{tech.label}
+								</a>
+							</HoverCardTrigger>
+							<HoverCardContent className="w-80 hidden md:flex">
+								<div className="flex justify-between space-x-4">
+									<Avatar>
+										<AvatarImage
+											src={`/${tech.label}.svg`}
+											alt={tech.label}
+											className={
+												tech.label === "nextjs"
+													? "bg-white border border-white"
+													: ""
+											}
+										/>
+										<AvatarFallback>{tech.label.slice(0, 2)}</AvatarFallback>
+									</Avatar>
+									<div className="space-y-1">
+										<h4 className="font-semibold">@{tech.label}</h4>
+										<p>{tech.description}</p>
+									</div>
+								</div>
+							</HoverCardContent>
+						</HoverCard>
+						<p className="inline">{index === techs.length - 1 ? "." : ","} </p>
+					</>
+				))}
 			</section>
 		</Layout>
 	);
