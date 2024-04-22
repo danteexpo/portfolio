b07cf109db37f29b63c9f66ed0a89c65563278d5
Dante <dante0im@gmail.com>
2024-04-21 23:56:21 -0300

    Fixed experience info

--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -14,28 +14,19 @@ export default function Work() {
           </h4>
         </div>
         <p>
-          • Contributed to the growth and enhancement of a betting assistant
-          application and worked on project features such as email templates for
-          clients, the implementation of notification systems built for
-          Telegram, email and web using the latest technologies.
+          • Contributed to the growth and enhancement of a betting assistant application and worked on project features such as email templates for clients, the implementation of notification systems built for Telegram, email and web using the latest technologies.
         </p>
         <p>
-          • Created, prototyped and applied UI/UX designs, actively participated
-          in product, design and technical discussions with team members.
+          • Created, prototyped and applied UI/UX designs, actively participated in product, design and technical discussions with team members.
         </p>
         <p>
-          • Got involved in architecting and designing technical solutions,
-          conducted general testing and performed bug fixes and issue
-          investigations.
+          • Got involved in architecting and designing technical solutions, conducted general testing and performed bug fixes and issue investigations.
         </p>
         <p>
-          • Built and maintained continuous integration processes to streamline
-          development. Solved logical coding problems, interacted with API
-          endpoints and reduced SEO warnings/errors.
+          • Built and maintained continuous integration processes to streamline development. Solved logical coding problems, interacted with API endpoints and reduced SEO warnings/errors.
         </p>
         <p>
-          • Stayed open-minded and demonstrated a willingness to receive and
-          incorporate feedback.
+          • Stayed open-minded and demonstrated a willingness to receive and incorporate feedback.
         </p>
       </section>
       <Separator />
@@ -43,29 +34,20 @@ export default function Work() {
         <div>
           <h2 className="text-2xl font-bold">Swise Software Factory</h2>
           <h4 className="text-gray-500 dark:text-gray-400">
-            UI/UX Designer and Frontend Developer 03/2022 - 06/2023
+            UI/UX Designer and Frontend Developer 03/2021 - 06/2023
           </h4>
         </div>
         <p>
-          • Designed and presented interactive UX/UI prototypes that served as
-          the foundation for developing a dynamic data visualization system.
+          • Designed and presented light and dark interactive UX/UI prototypes that served as the foundation for developing landing pages and a dynamic data visualization system.
         </p>
         <p>
-          • Transformed an Excel-based system into a high-performance and
-          responsive web application, empowering users to seamlessly input data
-          and visualize it through tables and multiple charts.
+          • Transformed an Excel-based system into a high-performance and responsive web application, empowering users to seamlessly visualize their data through tables and multiple charts.
         </p>
         <p>
-          • Optimized the information input, access, viewing, and download
-          experience by leveraging resources from REST API endpoints.
+          • Developed ways for restricting sensible data ensuring secure access for users, filtering data tables, viewing statistics and downloading documents by leveraging resources from REST API endpoints.
         </p>
         <p>
-          • Implemented user-specific route restrictions, ensuring secure access
-          and personalized functionality.
-        </p>
-        <p>
-          • Spearheaded the implementation of both light and dark modes,
-          elevating the overall user experience.
+          • Spearheaded the implementation of both PDF and XLSX download personalized functionalities, elevating the overall user experience of a complex application.
         </p>
       </section>
     </Layout>
